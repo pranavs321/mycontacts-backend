fe13e09faed305da53aeacba565b3095686c7f3c
admins <pranav.shomys@gmail.com>
2025-08-05 20:19:43 +0530

    Fix password hashing and registration routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,24 @@
 const asyncHandler = require("express-async-handler");
-
+const bcrypt = require("bcrypt");
+const User=require("../models/userModel");
 //@desc Register a user
 //@route POST /api/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
+  const {username,email,password}=req.body;
+  if(!username || !email || !password){
+    res.status(400);
+    throw new Error("All felids are mandatory");
+  }  
+  const userAvailable=await User.findOne({email});
+  if(userAvailable){
+    res.status(400);
+    throw new Error("User already exits");
+  }
+
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password: ",hashedPassword)
   res.json({ message: "Register the user" });
 });
 
