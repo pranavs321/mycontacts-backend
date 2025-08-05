b68d04d04429a40031c0b3ccb91550a8320cb04c
admins <pranav.shomys@gmail.com>
2025-08-05 21:55:42 +0530

    JWT ACCESS TOKEN AND USER LOGIN

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,78 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
-const User=require("../models/userModel");
+const jwt=require("jsonwebtoken");
+const User = require("../models/userModel");
+
+
 //@desc Register a user
 //@route POST /api/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  const {username,email,password}=req.body;
-  if(!username || !email || !password){
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
     res.status(400);
-    throw new Error("All felids are mandatory");
-  }  
-  const userAvailable=await User.findOne({email});
-  if(userAvailable){
-    res.status(400);
-    throw new Error("User already exits");
+    throw new Error("All fields are mandatory");
   }
 
-  //Hash password
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log("Hashed Password: ",hashedPassword)
-  res.json({ message: "Register the user" });
+  console.log("Hashed Password:", hashedPassword);
+
+  // Create user
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log(`User created: ${user}`);
+
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
 });
 
 //@desc Login user
 //@route POST /api/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login user" });
+    const {email,password}=req.body;
+    if(!email || !password){
+        res.status(400);    
+        throw new Error("All feilds are mandatory!");
+    }
+    const user=await User.findOne({email});
+    //compare password with hashpassword
+    if (user && (await bcrypt.compare(password,user.password))){
+        const accessToken=jwt.sign({
+            user:{
+                username:user.username,
+                email:user.email,
+                id:user.id,
+            },
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn:"1m"
+        }
+    );
+    res.status(200).json({accessToken});
+    }else{
+        res.status(401);
+        throw new Error("Email and password is not valid");
+    }
 });
+
 
 //@desc Current user
 //@route POST /api/current
